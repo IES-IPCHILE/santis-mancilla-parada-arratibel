c78a935da357bd2435315bbc6cefb8f43b69e5b3
Matias Arratibel <matias.arratibel@gmail.com>
2020-11-18 20:59:10 -0300

    Proyecto inicial 2
    a

--- index.php
+++ index.php
@@ -1,27 +1,17 @@
-
-<?php
-    include 'upload.php'
-
-?>
-
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Guardar la wea</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
 </head>
-<meta charset="utf-8">
 <body>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-<div class="container">
+    
 
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        <label>Select Image File:</label>
-        <input type="file" name="image">
-        <input type="submit" name="submit" value="Upload">
-    </form>
-
-</div>
 
 </body>
-    <a href="http://localhost/testBlob/view.php">miau</a>   
 </html>
