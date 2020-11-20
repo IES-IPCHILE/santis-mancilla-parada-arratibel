5f1722b6b9b47d727cd5f66aa09e58433f49cf23
edo9924 <lalitoman99@users.noreply.github.com>
2020-11-20 16:56:09 -0300

    cambios

--- gallery.php
+++ gallery.php
@@ -13,27 +13,21 @@
 <body>
     
     <script></script>
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">        
         <div class="collapse navbar-collapse" id="collapsibleNavId">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <li class="nav-item">
+                    <a class="nav-link" href="/santis-mancilla-parada-arratibel/index.php">Inicio</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="/santis-mancilla-parada-arratibel/gallery.php">Galeria</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownId">
-                        <a class="dropdown-item" href="#">Action 1</a>
-                        <a class="dropdown-item" href="#">Action 2</a>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="/santis-mancilla-parada-arratibel/services.php">Servicios</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/santis-mancilla-parada-arratibel/aboutus.php">Conocenos</a>
+                </li>   
                 </ul>
             <form class="form-inline my-2 my-lg-0">
             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#inicio">Iniciar Sesion</button>&nbsp;&nbsp;&nbsp;
