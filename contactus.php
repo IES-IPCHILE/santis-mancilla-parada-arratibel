4b22577cba072f9c1b8f634d3958cc9537b3b125
Matias Arratibel <matias.arratibel@gmail.com>
2020-11-25 11:08:06 -0300

    Nav tag actualizado
    con el inicio de sesion y registro en todas las paginas

--- contactus.php
+++ contactus.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+
+require("./resources/conexion.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,63 +18,168 @@
 </head>
 
 <body>
-
   <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <div class="collapse navbar-collapse" id="collapsibleNavId">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="index.php">Inicio</a>
+          <a class="nav-link" href="./index.php">Inicio</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="gallery.php">Galeria</a>
+          <a class="nav-link" href="./gallery.php">Galeria</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="services.php">Servicios</a>
+          <a class="nav-link" href="./services.php">Servicios</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="aboutus.php">Quiénes somos</a>
+          <a class="nav-link" href="./aboutus.php">Quiénes somos</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="contactus.php">Contacto</a>
+          <a class="nav-link" href="./aboutus.php">Contacto</a>
         </li>
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#inicio">Iniciar Sesion</button>&nbsp;&nbsp;&nbsp;
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#registro">Registrar</button>
-      </form>
+
+      <?php
+
+      if (!isset($_SESSION["id_user"])) {
+        echo
+          "<form class='form-inline my-2 my-lg-0'>
+          <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#i'>Iniciar Sesion</button>
+          </form>";
+      } else {
+        echo
+          "<form method='post' class='form-inline my-2 my-lg-0' action='./resources/iniciar_sesion.php'>
+          <button type='submit' class='btn btn-primary' name='logout'>Cerrar sesion</button>
+        </form>";
+      }
+
+      ?>
+
     </div>
-    <div class="modal fade" id="inicio">
-      <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal fade" id="i">
+      <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
 
           <!-- cabecera del diálogo -->
           <div class="modal-header">
-            <h4 class="modal-title">Iniciar Sesion</h4>
+            <ul class="nav nav-pills" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" href="#inicio" id="inicio-pills" data-toggle="pill" role="tab" aria-controls="inicio" aria-selected="true">Iniciar Sesion</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#registro" id="registro-pills" data-toggle="pill" role="tab" aria-controls="registro" aria-selected="false">Registrarse</a>
+              </li>
+            </ul>
             <button type="button" class="close" data-dismiss="modal">X</button>
           </div>
+          <script>
+            $(document).ready(function() {
+              var password = $("#register-password"),
+                confirm_password = $("#repeat-password");
 
+              function validatePassword() {
+
+                if (password.val() != confirm_password.val()) {
+                  confirm_password[0].setCustomValidity("Passwords Don't Match");
+                  confirm_password[0].reportValidity()
+                } else {
+                  confirm_password[0].setCustomValidity('');
+                }
+              }
+              password.change(validatePassword);
+              confirm_password.keyup(validatePassword);
+
+            })
+          </script>
           <!-- cuerpo del diálogo -->
           <div class="modal-body">
             <div class="container-fluid">
-              <form>
-                <div class="form-group row">
-                  <label for="usuario" class="col-lg-3 col-form-label">usuario:</label>
-                  <div class="col-lg-9">
-                    <input type="text" class="form-control" id="usuario">
-                  </div>
+
+              <div class="tab-content">
+                <div class="tab-pane fade show active" id="inicio" role="tabpanel" aria-labelledby="inicio-pills">
+                  <form method="POST" action="./resources/iniciar_sesion.php">
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Correo</label>
+                      <div class="col-lg-9">
+                        <input type="email" class="form-control" name="login-correo">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="clave" class="col-lg-3 col-form-label">Contraseña:</label>
+                      <div class="col-lg-9">
+                        <input type="password" class="form-control" name="login-password">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="offset-lg-3 col-lg-9">
+                        <button type="submit" class="btn btn-primary" name="login">Ingresar</button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-                <div class="form-group row">
-                  <label for="clave" class="col-lg-3 col-form-label">Contraseña:</label>
-                  <div class="col-lg-9">
-                    <input type="password" class="form-control" id="clave">
-                  </div>
+                <div class="tab-pane fade" id="registro" role="tabpanel" aria-labelledby="registro-pills">
+                  <form method="POST" action="./resources/iniciar_sesion.php">
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Nombre Completo:</label>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="nombre" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Correo:</label>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="correo">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Nombre de Usuario:</label>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="usuario" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="clave" class="col-lg-3 col-form-label">Contraseña:</label>
+                      <div class="col-lg-9">
+                        <input type="password" class="form-control" name="register-password" id="register-password" require>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="clave" class="col-lg-3 col-form-label">Repetir Contraseña:</label>
+                      <div class="col-lg-9">
+                        <input type="password" class="form-control" name="repeat-password" id="repeat-password" require>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Direccion:</label required>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="direccion" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">RUN:</label>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="run" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Fecha de nacimiento:</label>
+                      <div class="col-lg-9">
+                        <input type="date" class="form-control" name="fecha_nac" id="fecha_nac" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="usuario" class="col-lg-3 col-form-label">Fono (+56):</label>
+                      <div class="col-lg-9">
+                        <input type="text" class="form-control" name="fono" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <div class="offset-lg-3 col-lg-9">
+                        <button type="text" class="btn btn-primary" name="register" id="register">Registrar</button>
+                      </div>
+                    </div>
+                  </form>
                 </div>
-                <div class="form-group row">
-                  <div class="offset-lg-3 col-lg-9">
-                    <button type="submit" class="btn btn-primary">Ingresar</button>
-                  </div>
-                </div>
-              </form>
+              </div>
             </div>
           </div>
           <div class="modal-footer">
@@ -77,50 +188,6 @@
 
         </div>
       </div>
-
-    </div>
-    <div class="modal fade" id="registro">
-      <div class="modal-dialog modal-lg modal-dialog-centered">
-        <div class="modal-content">
-
-          <!-- cabecera del diálogo -->
-          <div class="modal-header">
-            <h4 class="modal-title">Iniciar Sesion</h4>
-            <button type="button" class="close" data-dismiss="modal">X</button>
-          </div>
-
-          <!-- cuerpo del diálogo -->
-          <div class="modal-body">
-            <div class="container-fluid">
-              <form>
-                <div class="form-group row">
-                  <label for="usuario" class="col-lg-3 col-form-label">Usuario:</label>
-                  <div class="col-lg-9">
-                    <input type="text" class="form-control" id="usuario">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <label for="clave" class="col-lg-3 col-form-label">Contraseña:</label>
-                  <div class="col-lg-9">
-                    <input type="password" class="form-control" id="clave">
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="offset-lg-3 col-lg-9">
-                    <button type="submit" class="btn btn-primary">Registrar</button>
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-          </div>
-
-        </div>
-      </div>
-
-    </div>
 
   </nav>
   <div id="carouselId" class="carousel slide" data-ride="carousel">
